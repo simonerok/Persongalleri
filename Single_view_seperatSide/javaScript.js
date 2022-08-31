c365fc8f1264d2dd01ec12199c0b3d71ace21c41
Simone <ssimone12@gmail.com>
2022-08-31 18:54:58 +0200

    virker stadig ikke laver ny mappe

--- Single_view_seperatSide/javaScript.js
+++ Single_view_seperatSide/javaScript.js
@@ -1,35 +1,38 @@
-/* vi laver en søger via urlSearchParams og urlparamams*/
+/* vi laver en søger via urlSearchParams og urlparamams som finder vores id*/
 const urlParams = new URLSearchParams(window.location.search);
-let id = urlParams.get("id");
+const id = urlParams.get("id");
 console.log({ id });
 
 const url "https://persongalleri-5d3e.restdb.io/rest/persongalleri/"+id;
-const key = "600fe9211346a1524ff12e31";
-
- /* vi henter/ fetcher igen */
-      const endpoint = "https://persongalleri-5d3e.restdb.io/rest/persongalleri ";
-      const mereinfo = {
-        headers: {
-          "x-apikey": "600fe9211346a1524ff12e31",
-        },
-      };
-
-async function hentData() {
-const respons = await fetch(endpoint, mereinfo);
- personer = await respons.json();
- method: `GET`, 
- mereinfo; {`Content-Type`;`application/json`}
-
-console.log(personer);
-vis(personer);
+let enPerson
+const myHeaders = {
+  "api-key": "600fe9211346a1524ff12e31"
 }
 
-function VisPerson (id){
-    console.log(id);
-    const enPerson =document.querySelector("person_id");
-    enPerson.querySelector("h2").textContent = personData.fornavn + " " + personData.efternavn
-    enPerson.querySelector("img").src = "../billeder/" + personData.billede;
-    enPerson.querySelector("img").alt = personData.billede;
+console.log("ID", id);
+document.addEventListener("DOMContentLoaded", hentData);
+
+async function hentData(){
+  const mereInfo = await fetch(`https://persongalleri-5d3e.restdb.io/rest/persongalleri/${id}`, {
+    headers: myHeaders
+  });
+enPerson = await mereInfo.json();
+
+console.log("personer", enPerson)
+VisPerson(enPerson);
 }
-VisPerson();
-hentData();
+
+function VisPerson (){
+   enPerson.querySelector("h2").textContent = enPerson.fornavn + " " + enPerson.efternavn
+    enPerson.querySelector(".person_id").src = "../billeder/" + enPerson.billede;
+    enPerson.querySelector(".person_id").alt = enPerson.billede;
+    document.querySelector("button").addEventListener("click", tilbageKnap);
+}
+
+
+function tilbageKnap (){
+  history.back();
+   }
+
+/* hentData();
+ */
